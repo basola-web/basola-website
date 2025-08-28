3b2218e00a8ab59487ec24b413f2d0cdcd13859b
slngnr00 <langner.simon@gmx.de>
2025-08-28 03:27:09 -0400

    chore: switch to node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://basola.example',
   integrations: [tailwind()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: node({ mode: 'standalone' }),
   i18n: {
     defaultLocale: 'en',
     locales: ['en','de'],
