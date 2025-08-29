64cfcfab0212f85f7ebe81a1201d0f2472606965
slngnr00 <langner.simon@gmx.de>
2025-08-29 12:38:26 -0400

    feat: introduce animated solution section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
         basola: { brown: '#6A2D0F', yellow: '#FFB703' },
       },
       boxShadow: {
-        glow: '0 0 0.75rem rgba(255, 183, 3, .55)',
+        glow: '0 0 0.75rem rgba(255,183,3,.45)',
       },
       transitionDuration: {
         1500: '1500ms',
